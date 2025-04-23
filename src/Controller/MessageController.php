aae2cb7170469500f4e7ab0471a520d0070d1251
souhajomaa <souhajomaa1412@gmail.com>
2025-04-23 23:03:21 +0100

    chat commit

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -2,20 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Conversation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/message', name: 'app_message')]
-
-final class MessageController extends AbstractController
+class MessageController extends AbstractController
 {
-    #[Route('/{id}', name: 'getMessages')]
-
-    public function index(): Response
+    #[Route('/{id}', name: 'message.getMessages')]
+    public function index(Request $request, Conversation $conversation): Response
     {
+        $this->denyAccessUnlessGranted('view', $conversation);
+
         return $this->render('message/index.html.twig', [
             'controller_name' => 'MessageController',
+            'conversation' => $conversation,
         ]);
     }
 }
